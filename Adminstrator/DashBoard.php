352009d59dada00c5e9e80687a23ca709faba783
Badrul Amin <74086349+KingBard77@users.noreply.github.com>
2022-03-23 15:13:02 +0800

    init

--- Adminstrator/DashBoard.php
+++ Adminstrator/DashBoard.php
@@ -34,7 +34,7 @@ $employee_num = mysqli_num_rows($employee);
                 <div class="row">
                     <div class="col-12 col-xl-8 mb-4 mb-xl-0">
                         <h3 class="font-weight-bold">Welcome <?php echo" Mr. {$_SESSION["Username"]} "?></h3>
-                        <h6 class="font-weight-normal mb-0">All systems are running smoothly! You have <span
+                        <h6 class="font-weight-normal mb-0">You're in BurgerByte system! You have <span
                                 class="text-primary">3 unread alerts!</span></h6>
                     </div>
                     <div class="col-12 col-xl-4">
